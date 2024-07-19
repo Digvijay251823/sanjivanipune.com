7224a9a687e695b7e968def90475061829517f04
Digvijay25182316 <digvijays.edake2002@gmail.com>
2024-07-19 21:50:46 +0530

    commit

--- ConfigFetch.ts
+++ ConfigFetch.ts
@@ -1,2 +1,2 @@
-export const SERVER_ENDPOINT = "http://localhost:5000/sanjivani";
+export const SERVER_ENDPOINT = "http://3.7.55.13:5000/sanjivani";
 export const VOLUNTEER_ACCESS = process.env.VOLUNTEER_ACCESS;
